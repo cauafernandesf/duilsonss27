325e3c838501c92f16769f4ef3502fef21ccf3a3
SeuNomeNoGitHub <seuemail@exemplo.com>
2026-05-27 14:48:06 -0300

    config vercel

--- backend/server.js
+++ backend/server.js
@@ -326,10 +326,5 @@ Segue em anexo o sortimento selecionado.
 
 });
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-
-  console.log(`Servidor rodando na porta ${PORT}`);
-
-});
+export default app;
